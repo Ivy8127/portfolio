e8e02979e72444113d9aca955090ca6414625a43
Ivy Mboya <mboyaivy@gmail.com>
2021-04-20 15:58:54 +0300

    navbar finished

---
+++ app.js
@@ -0,0 +1,8 @@
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector('.nav-list');
+const Links = document.querySelectorAll('.nav-list li');
+
+hamburger.addEventListener('click',()=>{
+    navLinks.classList.toggle('open')
+
+})
